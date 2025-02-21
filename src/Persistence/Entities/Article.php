d3606021cebe67f100ad1418a4db3aba2e31b475
arakviel <insider.arakviel@gmail.com>
2025-02-21 19:32:32 +0200

    init

---
+++ src/Persistence/Entities/Article.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Insid\Acdemo\Persistence\Entities;
+
+use Insid\Acdemo\Persistence\Utils\GenericEntity;
+
+class Article extends GenericEntity
+{
+    private string $title;
+    private string $content;
+    private ?string $image = null;
+    private string $created_at = '';
+    private ?string $updated_at = null;
+
+    protected static function getTableName(): string
+    {
+        return 'articles';
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): void
+    {
+        $this->content = $content;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): void
+    {
+        $this->image = $image;
+    }
+
+    public function getCreatedAt(): string
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(string $created_at): void
+    {
+        $this->created_at = $created_at;
+    }
+
+    public function getUpdatedAt(): ?string
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(?string $updated_at): void
+    {
+        $this->updated_at = $updated_at;
+    }
+}
